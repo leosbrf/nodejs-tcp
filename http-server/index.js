99ba8b699459d9a394f6045a453943906225e5a0
Francisco Leonardo <leo.sbrf@gmail.com>
2020-02-04 12:30:36 -0300

    about connections (net) and request/response objects (http)

--- http-server/index.js
+++ http-server/index.js
@@ -1,7 +1,11 @@
 require("http")
   .createServer(function(req, res) {
+    //console.log(req.headers);
+    console.log(req.connection);
     res.writeHead(200, { "Content-Type": "image/png" });
-    
-    require("fs").createReadStream("image.png").pipe(res);
+
+    require("fs")
+      .createReadStream("image.png")
+      .pipe(res);
   })
   .listen(3000);
