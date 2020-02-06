628e2f577c772514b98a543ca0e4985f6aeba04f
FRANCISCO OLIVEIRA <leo.sbrf@gmail.com>
2020-02-05 21:56:02 -0300

    request methods and urls

--- http-form/index.js
+++ http-form/index.js
@@ -1,13 +1,20 @@
-require("http").createServer(function(req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(`
-      <form method="POST" action="/url">
-        <h1>My form</h1>
-        <fieldset>
-        <label>Personal information</label>
-        <p>What is your name?</p>
-        <input type=”text” name=”name”>
-        <p><button>Submit</button></p>
-      </form>
-      `);
-}).listen(3000);
+require("http")
+  .createServer(function(req, res) {
+    if (req.url == "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`
+        <form method="POST" action="/url">
+          <h1>My form</h1>
+          <fieldset>
+          <label>Personal information</label>
+          <p>What is your name?</p>
+          <input type=”text” name=”name”>
+          <p><button>Submit</button></p>
+        </form>
+        `);
+    } else if (req.url == "/url") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`You sent a <em>${req.method}</em> request`);
+    }
+  })
+  .listen(3000);
